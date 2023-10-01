dc4a66d8d30b29ac841a8c33d855915c1da43d94
Isac Kaik <isackaik1@gmail.com>
2023-10-01 16:55:25 -0300

    Update version

---
+++ aula-api-spring/src/test/java/br/com/aula/AulaApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
